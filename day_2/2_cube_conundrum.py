253c8a54b829daf941a6427ef587dce3851a6d31
mgzik <mgzik@eversis.com>
2024-06-14 13:38:12 +0200

    day 2 WIP

--- day_2/2_cube_conundrum.py
+++ day_2/2_cube_conundrum.py
@@ -1,8 +1,17 @@
 
-
+def read_file():
+    with open('day_2/input.txt', 'r') as file:
+        # Read the entire file content into a single string
+        return file.read()
 
 def main(input):
     print("Hello World!")
+    input = read_file();
+    for line in input.split("\n"):
+        if not line:
+            continue
+        else:
+            print(line)
 
 if __name__ == "__main__":
     main(input)
