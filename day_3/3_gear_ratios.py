5c1c0e4e94f841239963b89fc0313e022927c309
maciejgz <macgzi@gmail.com>
2024-06-14 18:24:05 +0200

    day 3 init

---
+++ day_3/3_gear_ratios.py
@@ -0,0 +1,16 @@
+## https://adventofcode.com/2023/day/3
+import re
+
+def read_file():
+    with open('day_3/input.txt', 'r') as file:
+        # Read the entire file content into a single string
+        return file.read()
+    
+    
+def main():
+    print("sass");
+
+    
+    
+if __name__ == "__main__":
+    main()
