785620c14166d574f4be52ced8e8207b3307aa6f
mgzik <mgzik@eversis.com>
2024-06-14 13:06:43 +0200

    day_1 finished

--- day_1/1_trebuchet.py
+++ day_1/1_trebuchet.py
@@ -10,12 +10,29 @@ treb7uchet
 def main(input):
     
     values = []
+    result = 0;
     
-    for line in input.split("\n"):
+    with open('day_1/input.txt', 'r') as file:
+        # Read the entire file content into a single string
+        file_content = file.read()
+    
+    for line in file_content.split("\n"):
         if not line:
             continue
         else:
-            print(line)
+            first = None
+            last = None
+            for char in line:
+                if char.isdigit():
+                    if first is None:
+                        first = int(char)
+                    last = int(char)
+            
+            number = last + 10 * first;
+            values.append(number)
+    for value in values:
+        result += value  
+    print(result)    
         
 if __name__ == "__main__":
     main(input)
