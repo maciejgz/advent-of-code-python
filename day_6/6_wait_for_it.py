922fbbf4bacc657933b13a49e07850a0aa9d0f03
mgzik <mgzik@eversis.com>
2024-06-17 16:22:51 +0200

    day 6 finished, 7 init

--- day_6/6_wait_for_it.py
+++ day_6/6_wait_for_it.py
@@ -6,7 +6,55 @@ def read_file():
         return file.read()
     
 def main():
-    result = 0   
+    result = 0
+    input = read_file();
+    input_chars = []
+    for line in input.split("\n"):
+        if not line:
+            input_chars.append('')
+        else:
+            input_chars.append(line)
+    times = get_times(input_chars)
+    distances = get_distances(input_chars)
+    print(times)
+    
+    
+    ways_to_win = []
+    i = 0;
+    for race in times:
+        ways_to_win_internal = 0;
+        for j in range(0, times[i]+1):
+            print(j)
+            if (get_distance_traveled(race - j, j) > distances[i]):
+                ways_to_win_internal += 1
+        ways_to_win.append(ways_to_win_internal)
+        i+=1
+        
+    result = 1;
+    for ways in ways_to_win:
+        result *= ways
+        
+    print(result)
+        
+    
+
+def get_distance_traveled(time, distance):
+    return distance * time
+    
+            
+def get_times(input_line):
+    input_numbers = input_line[0].replace('Time: ', '').split()
+    for i in range(len(input_numbers)):
+        input_numbers[i] = int(input_numbers[i])
+    return input_numbers
+
+
+def get_distances(input_line):
+    input_numbers = input_line[1].replace('Distance: ', '').split()
+    for i in range(len(input_numbers)):
+        input_numbers[i] = int(input_numbers[i])
+    return input_numbers
+    
     
 if __name__ == "__main__":
     main()
---
+++ day_7/7_camel_cards.py
@@ -0,0 +1,18 @@
+
+
+def read_file():
+    with open("day_7/input.txt", "r") as file:
+        return file.read()
+    
+def main():
+    result = 0
+    input = read_file();
+    input_chars = []
+    for line in input.split("\n"):
+        if not line:
+            input_chars.append('')
+        else:
+            input_chars.append(line)
+    
+if __name__ == "__main__":
+    main()
